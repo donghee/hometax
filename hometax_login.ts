d94a3951f36bc483eea220eeb5f905bb14cb8c94
Donghee Park <dongheepark@gmail.com>
2026-09-23 09:08:28 +0900

    Use hometax_login.ts to check correct signin of business account

--- hometax_login.ts
+++ hometax_login.ts
@@ -1,7 +1,7 @@
 import { launchContext, loginWithRetry, getBusinessName, logout, authFile } from './hometax_common.ts';
 
 (async () => {
-  const context = await launchContext({ headless: false, timeout: 60000 * 60 });
+  const context = await launchContext({ headless: true, timeout: 60000 * 60 });
   const page = await context.newPage();
   if (await page.getByRole('link', { name: '로그아웃', exact: true }).isVisible()) {
     console.log('Logging out of current session...');
@@ -11,12 +11,13 @@ import { launchContext, loginWithRetry, getBusinessName, logout, authFile } from
   await loginWithRetry(page);
   await getBusinessName(page);
 
-  console.log('Login successful, navigating to copy invoice page...');
-  await page.getByRole('link', { name: '계산서·영수증·카드' }).click();
-  await page.getByRole('link', { name: '반복/복사 발급' }).click();
-  await page.getByRole('link', { name: '전자(세금)계산서 복사발급' }).click();
-  await page.getByRole('button', { name: '2개월' }).click();
-  await page.getByRole('button', { name: '조회' }).click();
+  // console.log('Login successful, navigating to copy invoice page...');
+  // await page.getByRole('link', { name: '계산서·영수증·카드' }).click();
+  // await page.getByRole('link', { name: '반복/복사 발급' }).click();
+  // await page.getByRole('link', { name: '전자(세금)계산서 복사발급' }).click();
+  // await page.getByRole('button', { name: '2개월' }).click();
+  // await page.getByRole('button', { name: '조회' }).click();
 
   await context.storageState({ path: authFile });
+  await context.close();
 })();
